9064d52af8fa654b5063c443a83a6ae28b874241
iklubov <iklubov@gmail.com>
2021-04-14 22:35:28 +0300

    completion for bindings

--- src/main/java/org/intellij/sdk/language/UssCompletionContributor.java
+++ src/main/java/org/intellij/sdk/language/UssCompletionContributor.java
@@ -9,15 +9,31 @@ import org.jetbrains.annotations.NotNull;
 
 public class UssCompletionContributor extends CompletionContributor {
 
+    public String[] bindings = {"controller","child","childParentScope","instance","event","dispatch","dispatchDelayReset","style","class","sync","repeat","repeatCache","repeatObject","dataRefDH","watchDH","entityDH"
+            ,"firstEntityDH","handleEventDH","fxInstance","mrMeeseeks","collectionDH","collectionDHById","collectionRepeatDH","collectionDesign","collectionFields","primaryEntityDH","clikList","draggableWindow"
+            ,"draggable","droppable","resize","appear","fade","transition","timestampTween","textCountdown","pluralText","tooltip","popup","popupNoCache","menu","blurLayer","blurMap","request","action","focus","sequence"
+            ,"feature","catch","catchProperty","var","watch","actionIsDisplay","scopeHoldRepeat","substitute","scopeTrace","changeDispatch","countdown","file","imeEnable","linearChart","eventSequence","contains","levelToFeature"
+            ,"timeFormat","serverTime","generator","generatorDH","clock","inoutAction","soundOn","blockSize","stageCoord","scrollController","scrollControllerCentered","catchDH","keyboard","debugWrite","debugRead"
+            ,"debugReadAll","debugSend","debugReceive","debugRewrite","debugTypeOf","debugScope","debugDataProvider","debugBlockInfo","debugScopeDraw","concat","colorTransform","clipboard","resource","slice","mc","objectUnderPoint"
+            ,"restrictFeedback","perFrameUpdate","indexOf","atlasText","timeline","battleHint","lag","makeScreenshot","directEvent","visible"};
     public UssCompletionContributor() {
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(UssTypes.BINDING),
+        // first iteration, because  parameters.getPosition() returns wrong token types sometimes
+        // but it works for binding prop so we have hardcode here yet
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(UssTypes.BINDING_PROP),
                 new CompletionProvider<CompletionParameters>() {
                     public void addCompletions(@NotNull CompletionParameters parameters,
                                                @NotNull ProcessingContext context,
                                                @NotNull CompletionResultSet resultSet) {
-                        resultSet.addElement(LookupElementBuilder.create("Hello"));
+                        for (String bind : bindings)
+                            resultSet.addElement(LookupElementBuilder.create(bind));
                     }
                 }
         );
+    }
+
+    @Override
+    public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+        super.fillCompletionVariants(parameters, result);
+        //System.out.println(parameters.getPosition());
     }
 }
